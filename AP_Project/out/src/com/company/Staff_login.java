d3fef37b8f8d1fdc6bfac63cc1106d95d812454d
Rupal-IIITD <rupal15081@iiitd.ac.in>
2017-02-26 15:24:25 +0530

    Update Staff_login.java

--- AP_Project/out/src/com/company/Staff_login.java
+++ AP_Project/out/src/com/company/Staff_login.java
@@ -67,3 +67,118 @@ public class Staff_login {
 
             }
         });
+		JButton btnSendRequests = new JButton("Send Logistics Requests");
+		btnSendRequests.setBounds(192, 64, 180, 33);
+        btnSendRequests.setBackground(Color.LIGHT_GRAY);
+		frame.getContentPane().add(btnSendRequests);
+        btnSendRequests.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                BufferedWriter output=null;
+                try {
+                    output = new BufferedWriter(new FileWriter("Requests_logistics.txt", true));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                if(output!=null){
+                    try {
+                        output.append("logistics request from Staff");
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    try {
+                        output.newLine();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                try {
+                    output.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+		
+		JButton btnSendLeave = new JButton("Send Leave");
+		btnSendLeave.setBounds(192, 108, 180, 37);
+        btnSendLeave.setBackground(Color.LIGHT_GRAY);
+		frame.getContentPane().add(btnSendLeave);
+        btnSendLeave.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                BufferedWriter output=null;
+                try {
+                    output = new BufferedWriter(new FileWriter("LeaveRequests.txt", true));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                if(output!=null){
+                    try {
+                        output.append("leave request from Staff");
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    try {
+                        output.newLine();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                try {
+                    output.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        JButton btnUpdateStatusOf = new JButton("Update status of tasks");
+		btnUpdateStatusOf.setBounds(192, 156, 180, 37);
+        btnUpdateStatusOf.setBackground(Color.LIGHT_GRAY);
+		frame.getContentPane().add(btnUpdateStatusOf);
+        btnUpdateStatusOf.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //System.out.println("Here you are");
+                JTextField fField = new JTextField(20);
+                JTextField gField = new JTextField(20);
+                Box b = Box.createVerticalBox();
+                //System.out.println("Here you are");
+                b.add(new JLabel("task_ID"));
+                b.add(fField);
+                b.add(Box.createVerticalStrut(10));
+                b.add(new JLabel("status"));
+                b.add(gField);
+                b.add(Box.createVerticalStrut(20));
+                int result = JOptionPane.showConfirmDialog(null, b,
+                        "Please Enter these values", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            BufferedReader in_file = null;
+            BufferedWriter out = null;
+            try {
+                in_file = new BufferedReader(new FileReader("Task_report.txt"));
+                out = new BufferedWriter(new FileWriter("New_Task_report.txt", true));
+            } catch (FileNotFoundException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            String line, new_line;
+            String id = fField.getText();
+
+            LineNumberReader lnr = null;
+            lnr = new LineNumberReader(in_file);
+            try {
+                while ((line = lnr.readLine()) != null) {
+                    if (line.indexOf(id) != -1) {
+                        new_line = line.replaceAll("ongoing", gField.getText());
+                        out.append(new_line);
+                        out.newLine();
+                    }
+                    else{
+                        out.append(line);
+                        out.newLine();
+                    }
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+		
